5abe31819d4b7a9cf48fce401af56dd80764b854
aisleypay <paisley.singh@gmail.com>
2017-05-23 21:56:34 -0400

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,30 @@ function handlebarsSetup() {
 $(document).ready(function (){
   handlebarsSetup()
 });
+
+function searchRepositories(){
+  const search = $("#searchTerms").val()
+
+  $.get(`https://api.github.com/search/repositories?q=${search}`, function(data){
+    const src = document.getElementById("repository-template").innerHTML
+    var template = Handlebars.compile(src);
+    var repoList = template(data.items)
+    document.getElementById("results").innerHTML = repoList
+  }).fail(displayError())
+}
+
+function displayError(){
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
+
+function showCommits(element){
+   var owner = element.dataset.owner
+   var repo = element.dataset.repository
+
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, function(data){
+    const src = document.getElementById("commits-template").innerHTML
+    var template = Handlebars.compile(src);
+    var dataList = template(data)
+    document.getElementById("details").innerHTML = dataList
+  }).fail(displayError())
+}
